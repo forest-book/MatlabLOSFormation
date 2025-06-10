4642731fd0abe3f202bb5584ec5c2d8b40bc4f87
forest-book <124020491+forest-book@users.noreply.github.com>
2025-06-11 00:15:20 +0900

    Update main.py

--- PythonVersion/main/main.py
+++ PythonVersion/main/main.py
@@ -383,7 +383,7 @@ for loop in range(0, simulation_time):
                 lt = quadrotor.coordinate[:, loop, quad_leader_num] - quadrotor.coordinate[:, loop, np.where(quadrotor.attribute_num == i + 1)] + D
 
                 # 式(3.13)
-                
+                h = k01[i, 0] + k01[i, 1] / (1 + np.linalg.norm(lt))
 
 
 
